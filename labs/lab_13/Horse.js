1d7859f5a31c358e352bd30235def1e325e58eba
datmt13 <154951655+datmt13@users.noreply.github.com>
2024-03-19 13:04:29 +0700

    Update Horse.js

--- labs/lab_13/Horse.js
+++ labs/lab_13/Horse.js
@@ -1,9 +1,5 @@
 const Animal = require("./Animal")
 class Horse extends Animal {
-    constructor(name, maxSpeed){
-        super(name,maxSpeed);
-    }
-
     getSpeed(){
         return  super.getSpeed();
     }
