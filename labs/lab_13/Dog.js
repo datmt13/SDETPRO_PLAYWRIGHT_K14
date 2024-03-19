9583e740355fb67a20f291feaac8422c7acbe58a
datmt13 <154951655+datmt13@users.noreply.github.com>
2024-03-19 13:04:16 +0700

    Update Dog.js

--- labs/lab_13/Dog.js
+++ labs/lab_13/Dog.js
@@ -1,9 +1,5 @@
 const Animal = require("./Animal")
 class Dog extends Animal {
-    constructor(name, maxSpeed){
-        super(name,maxSpeed);
-    }
-
     getSpeed(){
       return  super.getSpeed();
     }
